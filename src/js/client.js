f5b8066a2f23ee1265ec294a7d5769c50aa1fde8
Mike Cooper <mikecooper@Mikes-MacBook-Pro.local>
2017-02-09 12:06:22 +0000

    Redux pattern working

--- src/js/client.js
+++ src/js/client.js
@@ -1,66 +1,12 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
 
-// import Favorites from "./pages/Favorites";
-// import Todos from "./pages/Todos";
-// import Layout from "./pages/Layout";
-// import Settings from "./pages/Settings";
+import Layout from "./components/Layout"
+import store from "./store"
 
-// const app = document.getElementById('app');
+const app = document.getElementById('app')
 
-// ReactDOM.render(
-//   <Router history={hashHistory}>
-//     <Route path="/" component={Layout}>
-//       <IndexRoute component={Todos}></IndexRoute>
-//       <Route path="favorites" component={Favorites}></Route>
-//       <Route path="settings" component={Settings}></Route>
-//     </Route>
-//   </Router>,
-// app);
-
-import { applyMiddleware, createStore } from "redux";
-
-const reducer = (initialState=0, action) => {
-  if (action.type === "INC") {
-    return initialState + 1;
-  } else if (action.type === "DEC") {
-    return initialState - 1;
-  } else if (action.type === "MULT") {
-    throw new Error("AHHHH!!");
-  }
-  return initialState;
-}
-
-const logger = (store) => (next) => (action) => {
-  console.log("Logged", action);
-  return next(action);
-};
-
-
-const errorHandler = (store) => (next) => (action) => {
-  try {
-    return next(action);
-  } catch(e) {
-    console.log("ERROR!", e);
-  }
-};
-
-const middleware = applyMiddleware(
-  logger,
-  errorHandler
-)
-const store = createStore(reducer, middleware)
-
-store.subscribe(() => {
-  console.log("store changed", store.getState());
-})
-
-store.dispatch({type: "INC"})
-store.dispatch({type: "INC"})
-store.dispatch({type: "INC"})
-store.dispatch({type: "DEC"})
-store.dispatch({type: "DEC"})
-store.dispatch({type: "DEC"})
-store.dispatch({type: "MULT"})
-store.dispatch({type: "DEC"})
+ReactDOM.render(<Provider store={store}>
+  <Layout />
+</Provider>, app);
