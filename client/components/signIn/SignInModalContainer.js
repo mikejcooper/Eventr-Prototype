98f7d17a2ef0370a46fa165ee8aeef72ad4f8c3c
Mike Cooper <mikecooper@Mikes-MacBook-Pro.local>
2017-03-15 14:41:01 +0000

    Modal half working

--- client/components/signIn/SignInModalContainer.js
+++ client/components/signIn/SignInModalContainer.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+import css from './main.css'
+import TextFieldGroup from '../common/TextFieldGroup';
+
+
 
 class SignInModalContainer extends React.Component { 
+
   render() {
     console.log(this.props.showModal)
     const modalInstance = (
       <div className="static-modal">
         <Modal.Dialog>
-
-
           <Modal.Body>
-            One fine body...
-          </Modal.Body>
+            <br></br>
+            <h3 class="maintext">Good to see you again!</h3>
+            <p class="subtext">We've been waiting to show you <br></br> all the new events we found.</p>
+            
+            <div class="login-buttons facebook-container">
+              <div class="login-button google-plus-container attached">
+                <a href="https://en.wikipedia.org/wiki/Facebook" class="">
+                  <span class="i-facebook icon"></span>
+                  <span class="text">SIGN IN</span>
+                </a>
+              </div>
+              <div class="login-button google-plus-container attached">
+                <a href="https://en.wikipedia.org/wiki/Google" class="">
+                  <span class="i-gplus icon"></span>
+                  <span class="text">SIGN IN</span>
+                </a>
+              </div>
+            </div>
 
+            <form onSubmit={this.onSubmit}>
+              <TextFieldGroup
+                label="Username"
+                field="username"
+              />
+               <TextFieldGroup
+                label="Password"
+                field="password"
+              />
+
+
+            </form>
+          </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.props.closeModal}>Close</Button>
             <Button bsStyle="primary" onClick={fun => console.log("here")}>Save changes</Button>
@@ -31,3 +63,4 @@ class SignInModalContainer extends React.Component {
   }
 }
 export default SignInModalContainer;
+
